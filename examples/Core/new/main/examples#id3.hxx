892e3ceef9dbf200845e90a37bb53be776ef4e46
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-31 17:07:47 +0100

    Create examples#id3.hxx

---
+++ examples/Core/new/main/examples#id3.hxx
@@ -0,0 +1,31 @@
+#include <curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  curl = curl_easy_init();
+  if(curl) {
+    /*
+     * Make the URL end with a trailing slash
+     */
+    curl_easy_setopt(curl, CURLOPT_URL, "ftp://ftp.example.com/");
+
+    res = curl_easy_perform(curl);
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+
+    if(CURLE_OK != res) {
+      /* we failed */
+      fprintf(stderr, "curl told us %d\n", res);
+    }
+  }
+
+  curl_global_cleanup();
+
+  return 0;
+}
